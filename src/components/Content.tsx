3fc02f699a7ff8103b26bd080a838120f7387730
Alejandro Rojas <kmachappy@pm.me>
2022-08-10 22:40:30 -0700

    nav and footer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,7 @@
 
 const Content = () => {
     return (
-        <div className="container grow mx-auto py-2">
+        <div className="container grow mx-auto p-6 sm:py-6">
             <h1>Content</h1>
         </div>
     );
